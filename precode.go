70cd493d3407dc7d6ec2c0a42de78bf7c374a820
Ahmed <ahmed-guho@inbox.ru>
2024-10-22 23:15:24 +0300

    итоговоый проект 6 спринта

--- precode.go
+++ precode.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -41,12 +43,102 @@ var tasks = map[string]Task{
 
 // Ниже напишите обработчики для каждого эндпоинта
 // ...
+// Обработчик для получения всех задач
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	// сериализуем данные из слайса tasks
+	resp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// в заголовок записываем тип контента, у нас это данные в формате JSON
+	w.Header().Set("Content-Type", "application/json")
+	// так как все успешно, то статус OK
+	w.WriteHeader(http.StatusOK)
+	// записываем сериализованные в JSON данные в тело ответа
+	w.Write(resp)
+}
+
+// Обработчик для отправки задачи на сервер
+func postTasks(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	tasks[task.ID] = task
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	task, ok := tasks[id]
+	if !ok {
+		http.Error(w, "Задача не найдена", http.StatusNotFound)
+		return
+	}
+
+	resp, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+// Обработчик для удаления задачи по её ID.
+func deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	// Берем ID из параметра маршрута
+	id := chi.URLParam(r, "id")
+
+	// Проверяем, существует ли задача с таким ID
+	if _, exists := tasks[id]; !exists {
+		http.Error(w, "Задача не найдена", http.StatusBadRequest)
+		return
+	}
+
+	// Удаляем задачу
+	delete(tasks, id)
+
+	// Возвращаем статус 200 OK
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	resp := map[string]string{"message": "Task deleted successfully"}
+	json.NewEncoder(w).Encode(resp)
+}
 
 func main() {
 	r := chi.NewRouter()
 
 	// здесь регистрируйте ваши обработчики
 	// ...
+	// регистрируем в роутере эндпоинт `/tasks` с методом GET, для которого используется обработчик `getTasks`
+	r.Get("/tasks", getTasks)
+
+	// // регистрируем в роутере эндпоинт `/tasks` с методом POST, для которого используется обработчик `postTasks`
+	r.Post("/tasks", postTasks)
+
+	// регистрируем в роутере эндпоинт `/task/{id}` с методом GET, для которого используется обработчик `getTask`
+	r.Get("/tasks/{id}", getTask)
+
+	// Регистрация обработчика для удаления задачи
+	r.Delete("/tasks/{id}", deleteTaskHandler)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
